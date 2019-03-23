edcf6076c519f43e557efbb6b85d76d65b8fd185
Akanksha <sinhaakanksha285@gmail.com>
2019-03-23 22:06:55 +0530

    Goal module added

---
+++ Final/src/java/Servlets/AjaxHelper.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+import java.util.List;
+import java.util.ArrayList;
+import Model.*;
+import Converter.*;
+import DAO.*;
+import Listener.*;
+import Utility.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Akanksha
+ */
+public class AjaxHelper extends HttpServlet {
+public AjaxHelper(){
+         super();
+}
+
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String s=request.getParameter("q");
+        List<Book> l=new ArrayList<Book>();
+        l=Getbooks.GetBooks(s); 
+        request.getSession().setAttribute("Books", l);
+        response.sendRedirect("g.jsp");
+       
+       // processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        doGet(request,response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Final/src/java/Testers/DictionaryTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Testers;
+import DAO.*;
+import Model.*;
+import com.mongodb.MongoClient;
+import java.net.UnknownHostException;
+import java.util.List;
+
+/**
+ *
+ * @author Sakshi Sinha
+ */
+public class DictionaryTest 
+{
+public static void main(String[] args) throws UnknownHostException 
+ {
+   Dictionary d=new Dictionary();
+   d.setWordId("NASCENT11234");
+   d.setWord("Damn");
+   d.setMeaning("Does Not Work");
+   MongoClient mongo = new MongoClient("localhost", 27017);
+   DictionaryDAO dictDAO = new DictionaryDAO(mongo);
+   //Create
+   dictDAO.createDictionary(d);
+   System.out.println("Word Added Successfully with id="+d.getWordId()+","+d.getWord()+","+d.getMeaning());
+   List<Dictionary> list=dictDAO.readAllWords();
+   for(Dictionary us:list)
+   {
+       System.out.println(us.getWordId()+","+us.getWord()+","+us.getMeaning());
+   }
+   List<Dictionary> lists=dictDAO.readWords(d);
+     for(Dictionary us:lists)
+   {
+       System.out.println(us.getWordId()+","+us.getWord()+","+us.getMeaning());
+   }
+          
+ }    
+}
