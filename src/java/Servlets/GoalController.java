d923007796c7f197ce83c6d6012d1703c73a7bf1
AasthaSahay <47293369+AasthaSahay@users.noreply.github.com>
2019-04-23 14:31:55 +0530

    Update GoalController.java

--- src/java/Servlets/GoalController.java
+++ src/java/Servlets/GoalController.java
@@ -17,10 +17,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 /**
  *
